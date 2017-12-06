90ba04193f3bb2210581b490201ff58eddc336b9
Kevin <kevin98gray@gmail.com>
2017-12-06 23:03:38 +0800

    Add high score to main menu

--- Stack/Assets/Scripts/ViewHandler/ViewAnimation/MainMenuScreen.cs
+++ Stack/Assets/Scripts/ViewHandler/ViewAnimation/MainMenuScreen.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuScreen : View {
+
+    [SerializeField] Text highScoreText;
+
+    private const string KEY_HIGH_SCORE = "HighScore";
 
 	void Awake() {
 
@@ -11,7 +16,7 @@ public class MainMenuScreen : View {
 
 	// Use this for initialization
 	void Start () {
-		
+		this.GetHighScore();
 	}
 
 	public void OnPlayClicked() {
@@ -21,4 +26,8 @@ public class MainMenuScreen : View {
 	public void OnQuitClicked() {
 		Application.Quit();
 	}
+
+    private void GetHighScore() {
+        this.highScoreText.text = PlayerPrefs.GetInt(KEY_HIGH_SCORE, 0).ToString();
+    }
 }
